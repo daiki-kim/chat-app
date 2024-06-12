ee9b9ebea36f06c226e5a03482a8d0d3c5b9dccb
Daiki Kim <kimdaiki5021@gmail.com>
2024-06-13 07:29:13 +0900

    Create post and get routers

---
+++ pkg/api/router.go
@@ -0,0 +1,32 @@
+package api
+
+import "net/http"
+
+func NewRouter() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/messages", handleMessages)
+	mux.HandleFunc("/messages/", handleMessageByID)
+
+	return mux
+}
+
+func handleMessages(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		CreateMessage(w, r)
+	case "GET":
+		GetAllMessages(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func handleMessageByID(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		GetMessageByID(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
