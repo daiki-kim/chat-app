0bcdb44adb4889c806ea3537542e90695b006575
Daiki Kim <kimdaiki5021@gmail.com>
2024-06-13 07:29:37 +0900

    Add new router setup

--- cmd/chat-app/main.go
+++ cmd/chat-app/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/daiki-kim/chat-app/pkg/api"
 	"github.com/daiki-kim/chat-app/pkg/models"
 	"github.com/daiki-kim/chat-app/pkg/redis"
 	"github.com/daiki-kim/chat-app/pkg/websocket"
@@ -16,12 +17,13 @@ func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
 
+	r := api.NewRouter()
 	http.HandleFunc("/ws", websocket.HandleConnections)
 
 	go websocket.HandleMessages()
 
 	log.Println("http server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal("error starting http server: ", err)
 	}
