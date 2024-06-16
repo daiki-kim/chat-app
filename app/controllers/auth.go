b7636516deff39838654d9b66471c9c687b285c1
Daiki Kim <kimdaiki5021@gmail.com>
2024-06-17 07:29:01 +0900

    Add controller funtions

--- app/controllers/auth.go
+++ app/controllers/auth.go
@@ -1,1 +1,54 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/daiki-kim/chat-app/app/models"
+	"github.com/daiki-kim/chat-app/app/services"
+	"github.com/daiki-kim/chat-app/pkg/logger"
+	"go.uber.org/zap"
+)
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	var requestBody struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		logger.Warn("failed to decode request body", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = services.RegisterUser(requestBody.Username, requestBody.Email, requestBody.Password)
+	if err != nil {
+		logger.Error("failed to register user", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "User created successfully"})
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		logger.Warn("failed to decode request body", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	token, err := services.LoginUser(user.Email, user.Password)
+	if err != nil {
+		logger.Error("failed to login user", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
