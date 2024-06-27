ba4a098dd9e9ff130ea8ba7f5108dd42b336e9e1
Daiki Kim <kimdaiki5021@gmail.com>
2024-06-27 22:03:16 +0900

    Add test setup for repositories/room.go

--- app/repositories/room_test.go
+++ app/repositories/room_test.go
@@ -1,1 +1,28 @@
 package repositories_test
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/daiki-kim/chat-app/app/models"
+	"github.com/daiki-kim/chat-app/pkg/tester"
+	"github.com/stretchr/testify/suite"
+)
+
+type RoomeTestSuite struct {
+	tester.DBSQLiteSuite
+	originalDB *sql.DB
+}
+
+func TestRoomTestSuite(t *testing.T) {
+	suite.Run(t, new(RoomeTestSuite))
+}
+
+func (suite *RoomeTestSuite) SetupSuite() {
+	suite.DBSQLiteSuite.SetupSuite()
+	suite.originalDB = models.DB
+}
+
+func (suite *RoomeTestSuite) TestCreateRoom() {
+
+}
