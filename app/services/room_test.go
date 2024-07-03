6023eecba4918b88426d9254cfe16c27bd34f071
Daiki Kim <kimdaiki5021@gmail.com>
2024-07-04 07:32:13 +0900

    Create test file

--- app/services/room_test.go
+++ app/services/room_test.go
@@ -1,1 +1,31 @@
 package services_test
+
+import (
+	"github.com/daiki-kim/chat-app/app/models"
+	"github.com/daiki-kim/chat-app/pkg/tester"
+	"github.com/stretchr/testify/suite"
+)
+
+type RoomServiceTestSuite struct {
+	suite.Suite
+	mockDB *tester.MockDB
+}
+
+func (suite *RoomServiceTestSuite) SetupSuite() {
+	suite.mockDB = new(tester.MockDB)
+	models.DB = suite.mockDB
+}
+
+func (suite *RoomServiceTestSuite) TearDownSuite() {
+	suite.mockDB.AssertExpectations(suite.T())
+}
+
+func (suite *RoomServiceTestSuite) TestCreateRoom_Success() {
+	roomName := "test Room"
+	ownerID := 1
+	userIDs := []int{2, 3}
+
+	// TODO: Add return room  data and add more test cases
+	suite.mockDB.On("Exec", "INSERT INTO rooms (name, owner_id) VALUES ($1, $2) RETURNING id", roomName, ownerID).Return(nil)
+
+}
